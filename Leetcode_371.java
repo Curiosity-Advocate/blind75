76b393e8c570fd92b496595b96d3dee91e7234cf
Mahan Ghobadi <mahan.ghobadi@accenture.com>
2026-07-23 00:18:40 +1000

    Sum of Two Integers ([Sum of Two Integers](https://leetcode.com/problems/sum-of-two-integers/))
    Time complexity is O(n) but given it's integer in practice it's O(1). The space complexity is O(1)
    
    Time spent: 24m 43s (1483s)

---
+++ Leetcode_371.java
@@ -0,0 +1,13 @@
+public class Leetcode_371 {
+    
+    public int getSum(int a, int b) {
+        
+        while(b != 0){
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        
+        return a;
+    }
+}
